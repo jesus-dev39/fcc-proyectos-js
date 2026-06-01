13bf5cb6d45d6a55db8b1300ca1342dd123d220c
jesus-dev39 <ruizplanajj@gmail.com>
2026-06-01 18:26:56 +0200

    Laboratorio - Funcion Booleanos

---
+++ Laboratorio/Funciones/funcionBooleanos.js
@@ -0,0 +1,14 @@
+function booWho(argumento){
+  return typeof argumento === "boolean";
+}
+
+booWho(true);
+booWho(false);
+booWho([1, 2, 3]);
+booWho([].slice);
+booWho({ "a": 1 });
+booWho(1);
+booWho(NaN);
+booWho("a");
+booWho("true");
+booWho("false");
